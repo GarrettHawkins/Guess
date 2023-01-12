dc9baa5a5d48852c18ad03eb2755bf84a5e92f81
Garrett Hawkins <hawkinsgarrett819@gmail.com>
2023-01-12 10:29:58 -0700

    Initial Commit

---
+++ Flash2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash2.0
+//
+//  Created by Hawkins, Garrett - Student on 1/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
