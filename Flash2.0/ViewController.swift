13cb1165e1df1f05d6b6e5855bdf7e49f0cba8e3
Garrett Hawkins <hawkinsgarrett819@gmail.com>
2023-01-13 10:46:27 -0700

    e

--- Flash2.0/ViewController.swift
+++ Flash2.0/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func myButton(_ sender: Any) {
+        
+    }
+    
 }
 
